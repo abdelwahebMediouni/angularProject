8803e9cd70aac4f42a3105c46f6ae65f5013c2e1
abdou <mediouni.abdelwaheb@gmail.com>
2019-02-18 11:34:25 +0100

    App complete

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
+
 var express = require('express');
 var app = express();
+// Add headers
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.header('Access-Control-Allow-Origin', "*");
+
+    // Request methods you wish to allow
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
 var MatiereController = require('./matiere/MatiereController');
 app.use('/matieres', MatiereController);
 
